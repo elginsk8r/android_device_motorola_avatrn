7dddf909982b0942ea945324249de00eb3516356
Nick Reuter <nreuter85@gmail.com>
2026-01-21 20:53:29 -0600

    avatrn: Switch to hardware/motorola fingerprint
    Change-Id: I7f2b1eca23e740cbdbf1fe84bb878a600582a17f

--- device.mk
+++ device.mk
@@ -50,7 +50,7 @@ PRODUCT_COPY_FILES += \
 
 # Fingerprint
 PRODUCT_PACKAGES += \
-    android.hardware.biometrics.fingerprint@2.3-service.moto_sm7435_fod
+    android.hardware.biometrics.fingerprint@2.3-service.moto
 
 # Init
 PRODUCT_PACKAGES += \
